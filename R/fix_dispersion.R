ef28415d86ae1cf039dd07bd75382d6884cd26aa
Tom Beesley <t.beesley@lancaster.ac.uk>
2020-09-02 23:22:44 +0100

    fix_dispersion almost working correctly updated data files

--- R/fix_dispersion.R
+++ R/fix_dispersion.R
@@ -4,9 +4,6 @@
 #'
 #'
 #' @param data dataframe with columns time, x, y, trial (the standardised raw data form for eyeproc)
-#' @param
-#' @param
-#' @param
 #'
 #' @return
 #' @export
@@ -17,57 +14,74 @@
 #' @import dplyr
 #' @import purrr
 #' @importFrom rlang .data
+#' @importFrom zoo na.trim
 #'
-fix_dispersion <- function(data, min_dur = 150, disp_tol = 30, report = FALSE) {
+fix_dispersion <- function(data, min_dur = 50, disp_tol = 30, report = FALSE) {
 
-  # might be best to turn the data into a list by trial and
-  # process it by applying another function over it
-
-
-
-
+  data <- split(data,data$trial) # create list from data by trial
+  data_fix <- map(data, trial_level_process(min_dur = min_dur, disp_tol = disp_tol))
+  data_fix <- do.call("rbind", data_fix)
 
 }
 
-trial_level_process <- function(data) {
+trial_level_process <- function(data, min_dur, disp_tol, round = TRUE) {
 
-  vals <- slice_sample(100)
+  # need to think about how I catch NAs in the raw data
+  # argument option to skip ahead to the next valid data, or average over NAs
 
-  return(vals)
+  # also need to make min_dur based on ms not number of samples
 
+  data[,1] <- data[,1] - as.numeric(data[1,1]) # start trial timestamps at 0
 
+  data <- na.trim(data) # remove leading and trailing NAs
+  first_ts <- 1 # first timestamp of window
+  last_ts <- min_dur # last timestamp of window
+
+  # preallocate a tibble to store fixations
+  n <- ceiling(nrow(data)/min_dur) # maximum possible fixations
+  fix_store <- data.frame(matrix(NA, nrow = n, ncol = 4))
+
+  new_fix <- FALSE
+  fix_cnt <- 0
+  while (last_ts <= nrow(data)) {
+
+    win <- data[first_ts:last_ts,]
+
+    disp <- max(dist(win[,2:3])) # compute distance on X and Y
+
+    if (disp <= disp_tol) {
+      # increase window size
+      last_ts <- last_ts + 1
+      new_fix <- TRUE
+      #browser()
+    } else {
+      # disp_tol has been exceeded
+      if (new_fix == TRUE){ # record the fixation
+        #browser()
+        fix_cnt <- fix_cnt + 1
+        fix_store[fix_cnt,1] <- as.numeric(win[1,1]) # start timestamp of the fixation
+        fix_store[fix_cnt,2] <- tail(win[,1],n=1) - win[1,1] # subtract first from last timestamp for duration
+        if (round == TRUE) {
+          fix_store[fix_cnt,3:4] <- round(colMeans(win[,2:3])) # x/y coordinate means
+        } else {
+          fix_store[fix_cnt,3:4] <- colMeans(win[,2:3]) # x/y coordinate means
+        }
+        first_ts <- last_ts + 1
+        last_ts <- first_ts + min_dur
+        new_fix = FALSE
+        #browser()
+      } else {
+        # move window by one timestamp
+        first_ts <- first_ts + 1
+        last_ts <- last_ts + 1
+      }
+    }
+  }
+  # tidy up fixation dataframe
+  fix_store <- na.trim(fix_store)
+  if (nrow(fix_store)>0) fix_store <- cbind(fix_store,data[1,4]) # gets the trial number from top row
+
+  return(fix_store)
 }
 
 
-# OLD MATLAB METHOD FOR DISPERSION FIXATIONS
-
-# curFirst = 1; curLast = ceil(DurThresh/Interval);
-#
-# newFix = false; fixCnt = 0;
-# while curLast <= size(EGdata,1)
-#
-#   Win = EGdata(curFirst:curLast,:);
-#
-#   dsp = [abs(max(Win(:,1)) - min(Win(:,1))) abs(max(Win(:,2)) - min(Win(:,2)))];
-#
-#   if dsp <= DispThresh
-#     % increase window
-#     curLast = curLast + 1;
-#     newFix = true;
-#   else
-#     if newFix == true
-#       % record as end of fixation
-#       fixCnt = fixCnt + 1;
-#       fixStore(fixCnt,1:2) = mean(Win(:,1:2),1); % X&Y
-#       fixStore(fixCnt,3) = size(Win,1)*Interval; % duration
-#       fixStore(fixCnt,4:5) = [Win(1,4) Win(end,4)]; % start/end timestamps
-#       curFirst = curLast + 1;
-#       curLast = curFirst + ceil(DurThresh/Interval);
-#       newFix = false;
-#     else
-#       % move window to right and start again
-#       curFirst = curFirst + 1;
-#       curLast = curLast + 1;
-#     end
-#   end
-# end
--- sketch_file.R
+++ sketch_file.R
@@ -1,20 +1,19 @@
+library(tidyverse)
 library(devtools)
 
-# # fixing up some example data from the saccades experiments
-#
-# d <- read_csv("inst/extdata/stim_P103.csv", col_names = FALSE)
-# d <- read_csv("data/example_raw_d1.csv", col_names = TRUE)
-#
-# colnames(d) <- c("time", "x", "y", "trial")
-#
-# example_raw_3 <- d %>%
-#   mutate(d, across(c(x,y), ~na_if(.,0))) %>%
-#   filter(trial <= 100)
-#
-# usethis::use_data(example_raw_1,example_raw_2,example_raw_3)
+# fixing up some example data from the saccades experiments
 
+d <- read_csv("inst/extdata/stim_P101_Exp6.csv", col_names = FALSE)
+d <- read_csv("inst/extdata/dec_C1_P104_Exp6.csv", col_names = FALSE)
+#d <- read_csv("data/example_raw_d1.csv", col_names = TRUE)
 
-use_package("magrittr")
+colnames(d) <- c("time", "x", "y", "trial")
+
+example_raw_psy <- d %>%
+  mutate(d, across(c(x,y), ~na_if(.,0))) %>%
+  filter(trial <= 100)
+
+usethis::use_data(example_raw_psy, overwrite = TRUE)
 
 
 
