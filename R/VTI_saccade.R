3193fe99918650fc8f37dabb4f0750b763c981bf
tombeesley <t.beesley@lancaster.ac.uk>
2021-09-14 17:01:49 +0100

    add smoothing

--- R/VTI_saccade.R
+++ R/VTI_saccade.R
@@ -1,9 +1,41 @@
-VTI_saccade <- function(data){
+#' Velocity threshold identification of saccades
+#'
+#' Takes a
+#'
+#' @param data
+#' @param sample_rate sample rate of the eye-tracker. If default of NULL, then it will be computed from the timestamp data and the number of samples
+#'
+#' @return
+#' @export
+#'
+#' @examples VTI_saccade()
+#'
+
+VTI_saccade <- function(data, sample_rate = NULL, ...){
+
+  if (is.null(sample_rate)) {
+    # estimate sample rate
+    time <- data$time[nrow(data)] - data$time[1]
+    sample_rate <- 1000/(time/nrow(data)) # total time taken / samples
+
+  }
 
 
+  x <- data$x
+  y <- data$y
 
+  d <- as.matrix(dist(cbind(x,y)))
 
+  d_diag <- diag(d[2:nrow(d),])
 
+  data_new <- cbind(data,
+                    distance = c(NA,d_diag))
+
+  data_new$distance <- dist_to_visual_angle(data_new$distance, ...)
+
+  data_new$vel <- data_new$distance*sample_rate # visual angle per second
+
+  return(data_new)
 
 
 }
--- sketch_pad_file.R
+++ sketch_pad_file.R
@@ -20,111 +20,96 @@ profvis ({
   a <- eyetools::fix_dispersion(example_raw_psy)
 })
 
-d <-
-  read_csv("EAS01_P106_EG.csv",
-           col_names = c("time", "x", "y", "trial"),
-           cols(.default = col_double())) %>%
-  mutate(across(c(x,y), ~ifelse(is.nan(.), NA, .))) # convert NaN to NA
+# d <-
+#   read_csv("EAS01_P106_EG.csv",
+#            col_names = c("time", "x", "y", "trial"),
+#            cols(.default = col_double())) %>%
+#   mutate(across(c(x,y), ~ifelse(is.nan(.), NA, .))) # convert NaN to NA
 
-d_test <- filter(d, trial == 158)
-
-fix <- fix_dispersion(d) # process fixations
 
 # get raw data
-t1_raw <- filter(example_raw_sac, trial == 2)
+t_raw <- filter(example_raw_sac, trial == 6)
 
 # process fixations
-t1_fix <- eyetools::fix_dispersion(t1_raw)
+t_fix <- eyetools::fix_dispersion(t_raw)
 
-# process saccades
-t1_sac <- detect_saccades(t1_raw, lambda = 15)
+t_raw <- interpolate(t_raw)
 
-# number the events
-t1_sac <-
-  cbind(t1_sac,
-        event_n = c(1,cumsum(abs(diff(t1_sac$saccade)))+1))
+loess_x <- loess(x ~ time, data = t_raw, span = 0.1, na.action = na.exclude)
+loess_y <- loess(y ~ time, data = t_raw, span = 0.1, na.action = na.exclude)
 
-# plot a subset
-t1_g <-
-  t1_sac %>%
-  filter(between(event_n, 1, 10)) %>%
-  mutate(event_n = as.factor(event_n))
+t_raw$smooth_x <- predict(loess_x)
+t_raw$smooth_y <- predict(loess_y)
 
-t1_g %>%
-  ggplot(aes(x = x, y = y)) +
-  geom_point(aes(colour = event_n,
-                 shape = saccade),
-             size = 6) +
-  scale_color_discrete()
+t_raw %>%
+  ggplot() +
+  geom_point(aes(x = time, y = x), size = 3, alpha = .2) +
+  geom_line(aes(x = time, y = smooth_x), colour = "red", size = 1)
 
-# how many samples do saccades typically have
-t1_sac %>%
-  filter(saccade == TRUE) %>%
-  group_by(event_n) %>%
-  summarise(n_samples = n()) %>%
-  ggplot(aes(n_samples)) +
-  geom_histogram()
+t_raw %>%
+  ggplot() +
+  geom_point(aes(x = time, y = y), size = 3, alpha = .2) +
+  geom_line(aes(x = time, y = smooth_y), colour = "blue", size = 1)
 
+t_sac_new <- VTI_saccade(t_raw, sample_rate = 300, dist_type = "pixel")
 
-# summarise saccade data
-rl <- rle(t1_sac$saccade)
-ends <- cumsum(rl$lengths)
-starts <- c(1,ends[1:length(ends)-1]+1)
+t_sac_new$distance <- dist_to_visual_angle(t_sac_new$distance)
 
+t_sac_new$vel <- t_sac_new$distance*300 # visual angle per second
 
-
-data.frame(saccade = rl$values,
-           ts_start = starts,
-           ts_end = ends)
-
-# temp VTI function
-
-VTI_saccade <- function(data,
-                        dist_type = "cm",
-                        view_dist_cm = 60,
-                        screen_width_cm = 51,
-                        screen_width_pixels = 1920,
-                        sample_rate = NULL){
-
-  if (is.null(sample_rate)) {
-    # estimate sample rate
-    time <- data$time[nrow(data)] - data$time[1]
-
-
-  }
-
-
-  x <- data$x
-  y <- data$y
-
-  d <- as.matrix(dist(cbind(x,y)))
-
-  d_diag <- diag(d[2:nrow(d),])
-
-  data_new <- cbind(data,
-                    distance = c(NA,d_diag))
-
-  data_new$distance <- distance_to_visual_angle(data_new$distance)
-
-
-}
-
-t1_raw <- interpolate(t1_raw)
-
-t1_sac_new <- VTI_saccade(t1_raw)
-
-t1_sac_new$distance <- distance_to_visual_angle(t1_sac_new$distance)
-
-t1_sac_new$vel <- t1_sac_new$distance*300 # visual angle per second
-
-t1_sac_new[2:nrow(t1_sac_new),] %>%
-  ggplot(aes(x = time, y = smooth(vel))) +
+t_sac_new[2:nrow(t_sac_new),] %>%
+  ggplot(aes(x = time, y = vel)) +
   geom_point()
 
 
 
 
-# AOI analysis
+# # process saccades (old version - from "saccades" package)
+# t1_sac <- detect_saccades(t1_raw, lambda = 15)
+#
+# # number the events
+# t1_sac <-
+#   cbind(t1_sac,
+#         event_n = c(1,cumsum(abs(diff(t1_sac$saccade)))+1))
+#
+# # plot a subset
+# t1_g <-
+#   t1_sac %>%
+#   filter(between(event_n, 1, 10)) %>%
+#   mutate(event_n = as.factor(event_n))
+#
+# t1_g %>%
+#   ggplot(aes(x = x, y = y)) +
+#   geom_point(aes(colour = event_n,
+#                  shape = saccade),
+#              size = 6) +
+#   scale_color_discrete()
+#
+# # how many samples do saccades typically have
+# t1_sac %>%
+#   filter(saccade == TRUE) %>%
+#   group_by(event_n) %>%
+#   summarise(n_samples = n()) %>%
+#   ggplot(aes(n_samples)) +
+#   geom_histogram()
+#
+#
+# # summarise saccade data
+# rl <- rle(t1_sac$saccade)
+# ends <- cumsum(rl$lengths)
+# starts <- c(1,ends[1:length(ends)-1]+1)
+#
+# data.frame(saccade = rl$values,
+#            ts_start = starts,
+#            ts_end = ends)
+
+# temp VTI function
+
+
+
+
+
+# AOI analysis testing
 
 AOIs <- data.frame(matrix(nrow = 3, ncol = 4))
 colnames(AOIs) <- c("x", "y", "width", "height")
