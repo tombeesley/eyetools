1ea64450f53b3186a4cf1fe14813c2abe8ef5c01
Matthew <matthew.ivory@yahoo.co.uk>
2024-09-27 19:18:42 +0100

    Update test-AOI_seq.R

--- tests/testthat/test-AOI_seq.R
+++ tests/testthat/test-AOI_seq.R
@@ -3,7 +3,7 @@ fix_d <- fixation_dispersion(example_raw_WM)
 # a first, basic (but CONFIRMATORY) test
 testthat::expect_vector(
   {AOI_seq(fix_d, eyetools::AOIs_WM)},
-  size = 427
+  size = 430
 )
 
 
