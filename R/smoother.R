00dfb45944ba649bc187baf5e1abd483df761ce7
Matthew <matthew.ivory@yahoo.co.uk>
2024-10-10 17:27:56 +0100

    enabled multi-ppt check

--- R/smoother.R
+++ R/smoother.R
@@ -4,6 +4,7 @@
 #'
 #' @param data A dataframe with raw data (time, x, y, trial) for one participant
 #' @param span From stats::loess. The parameter α which controls the degree of smoothing.
+#' @param participant_ID the variable that determines the participant identifier. If no column present, assumes a single participant
 #' @return a dataframe of the same shape as the input data
 #' @export
 #'
@@ -14,12 +15,35 @@
 #' @importFrom stats loess predict na.exclude
 #'
 
-smoother <- function(data, span = 0.1) {
+smoother <- function(data, span = 0.1, participant_ID = "participant_ID") {
 
-  data_s <- split(data, data$trial)
-  by_trial_data <- lapply(data_s, smoother_trial)
-  flat_trials <- do.call(rbind.data.frame, by_trial_data)
-  return(flat_trials)
+  #first check for multiple/single ppt data
+  if (participant_ID == 'participant_ID') {
+    if(is.null(data[['participant_ID']])) {
+      participant_ID = "participant_ID"
+      data <- cbind(data, participant_ID = c("NOT A VALID ID")) # just assign a value
+    }
+  } else {
+    if(is.null(data[[participant_ID]])) stop(paste0("No participant identifier column called '", participant_ID, "' detected"))
+  }
+
+  internal_smooth <- function(data, span) {
+
+    data_s <- split(data, data$trial)
+    by_trial_data <- lapply(data_s, smoother_trial)
+    flat_trials <- do.call(rbind.data.frame, by_trial_data)
+    return(flat_trials)
+  }
+
+  data <- split(data, data[[participant_ID]])
+  out <- lapply(data, internal_smooth, span)
+  out <- do.call("rbind.data.frame", out)
+  rownames(out) <- NULL
+
+
+  if (out[['participant_ID']][1] == "NOT A VALID ID") out[['participant_ID']] <- NULL
+
+  return(out)
 
 }
 
