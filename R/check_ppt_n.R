7cbc84d783076bb37b31675dcd059b62e671c1d0
Matthew <matthew.ivory@yahoo.co.uk>
2024-10-15 18:32:54 +0100

    Update check_ppt_n.R
    Added error catch for unspecified multi-ppt data

--- R/check_ppt_n.R
+++ R/check_ppt_n.R
@@ -1,7 +1,18 @@
+
 .check_ppt_n_in <- function(participant_ID, data) {
 
   if (participant_ID == 'participant_ID') {
+
     if(is.null(data[['participant_ID']])) {
+
+      #this checks for repeated non-consecutive values of trial or time. If they are non-consecutive, error because it is likely
+      #it should have the participant_ID specified
+      trial_rep <- duplicated(rle(data$trial)$values)
+      time_rep <- duplicated(rle(data$time)$values)
+
+      if (sum(trial_rep) > 0) stop("multiple duplicated trials detected. Have you forgotten to specify the participant_ID?")
+      if (sum(time_rep) > 0) stop("multiple duplicated timepoints detected. Have you forgotten to specify the participant_ID?")
+
       participant_ID = "participant_ID"
       data <- cbind(data, participant_ID = c("NOT A VALID ID")) # just assign a value
     }
