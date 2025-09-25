a452329075308c40f22781d1767058c35a90613d
Matthew <matthew.ivory@yahoo.co.uk>
2025-09-25 10:25:16 +0100

    update combine_eyes to include a progress bar

--- R/combine_eyes.R
+++ R/combine_eyes.R
@@ -7,49 +7,56 @@
 #'
 #' @param data raw data with columns time, left_x, left_y, right_x, right_y, and trial
 #' @param method either "average" or "best_eye" - see description.
-#'
+#' @param progress whether to provide a progress bar or not.
 #' @return a dataframe of x-2 variables (with left_x and right_x condensed to x, and left_y and right_y condensed to y) and the same number of observations as the input data
 #' @export
 #'
 #' @examples combine_eyes(HCL, method = "average")
 #'
 
-combine_eyes <- function(data, method = "average") {
-
-  if (method == "average") {
-
-    x <- rowMeans(data[,c('left_x','right_x')], na.rm = TRUE)
-    y <- rowMeans(data[,c('left_y','right_y')], na.rm = TRUE)
-
-  } else if (method == "best_eye") {
-
-    if (mean(is.na(data$left_x)) < mean(is.na(data$right_x))){
-      # left eye has fewer NAs
-      x <- data$left_x
-      y <- data$left_y
-    } else {
-      # right eye has fewer NAs
-      x <- data$right_x
-      y <- data$right_y
-    }
-
-  } else {
-
+combine_eyes <- function(data, method = "average", progress = TRUE) {
+  if (!method %in% c("average", "best_eye")) {
     stop("Unexpected input to parameter 'method'. Use 'average' or 'best_eye'.")
-
   }
-
-  data$left_x <- NULL
-  data$left_y <- NULL
-  data$right_x <- NULL
-  data$right_y <- NULL
-
-  data <- cbind(data,x, y)
-
-  data[data == 'NaN']=NA # convert any NaN (from mean()) to NA
-
+  
+  if (method == "average") {
+    if (progress) {
+      xy <- pbapply::pbapply(
+        data[, c("left_x", "right_x", "left_y", "right_y")],
+        1,
+        function(row) {
+          c(mean(row[1:2], na.rm = TRUE), mean(row[3:4], na.rm = TRUE))
+        }
+      )
+      x <- xy[1, ]
+      y <- xy[2, ]
+    } else {
+      x <- rowMeans(data[, c("left_x", "right_x")], na.rm = TRUE)
+      y <- rowMeans(data[, c("left_y", "right_y")], na.rm = TRUE)
+    }
+    
+  } else if (method == "best_eye") {
+    use_left <- mean(is.na(data$left_x)) < mean(is.na(data$right_x))
+    cols <- if (use_left) c("left_x", "left_y") else c("right_x", "right_y")
+    
+    if (progress) {
+      xy <- pbapply::pbapply(data[, cols], 1, identity)
+      x <- xy[1, ]
+      y <- xy[2, ]
+    } else {
+      x <- data[[cols[1]]]
+      y <- data[[cols[2]]]
+    }
+  }
+  
+  # cleanup (only written once)
+  data <- within(data, {
+    left_x <- left_y <- right_x <- right_y <- NULL
+  })
+  
+  data <- cbind(data, x, y)
+  data[data == "NaN"] <- NA
   data <- data.frame(data)
-
+  
   return(data)
-
 }
